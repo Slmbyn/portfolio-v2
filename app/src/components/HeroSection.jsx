acb9a8da704eb132f739e5e0d1750d5d002ff266
SelamB <selambeyene2@gmail.com>
2024-05-18 17:30:43 -0700

    fixes CV button size

--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -18,11 +18,11 @@ const HeroSection = () => {
                     <br></br>
                     <TypeAnimation
                         sequence={[
-                            'Selam',
+                            'Selam.',
                             4000,
-                            'a Software Engineer',
+                            'a Software Engineer.',
                             2000,
-                            'a Day Trader',
+                            'a Day Trader.',
                             2000,
                             'a Warriors Fan',
                             2000
@@ -40,7 +40,7 @@ const HeroSection = () => {
                         Hire Me
                     </button>
                     <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white mt-3'>
-                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-5'>
+                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                             Download CV
                         </span>
                     </button>
