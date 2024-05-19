39c1e661a31089a208199b63172a88e9f023580f
SelamB <selambeyene2@gmail.com>
2024-05-19 16:30:57 -0700

    adds about me tab content

--- app/src/components/AboutSection.jsx
+++ app/src/components/AboutSection.jsx
@@ -3,6 +3,44 @@ import React, {useTransition, useState} from 'react'
 import Image from "next/image"
 import TabButton from './TabButton'
 
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: 'skills',
+        content: (
+            <ul className='list-desc pl-2'>
+                <li>JavaScript</li>
+                <li>Python</li>
+                <li>React</li>
+                <li>Next.js</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>PostgreSQL</li>
+            </ul>
+        )
+    },
+    {
+        title: "Education",
+        id: 'education',
+        content: (
+            <ul className='list-desc pl-2'>
+                <li>General Assembly</li>
+                <li>San Jose State University</li>
+            </ul>
+        )
+    },
+    {
+        title: "Certifications",
+        id: 'certifications',
+        content: (
+            <ul className='list-desc pl-2'>
+                <li>Full Stack Software Engineer</li>
+                <li>Software Testing</li>
+            </ul>
+        )
+    },
+]
+
 function AboutSection() {
     const [tab, setTab] = useState('skills')
     const [isPending, startTransition] = useTransition()
@@ -16,7 +54,7 @@ function AboutSection() {
     <section className='text-white'> 
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
             <Image src='/images/about-image.png' width={500} height={500} />
-            <div>
+            <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                 <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
                 <p className='text-base lg:text-lg'>
                     I am a full stack web developer with a passion for creating
@@ -31,15 +69,16 @@ function AboutSection() {
                         {" "}
                         Skills{" "}
                     </TabButton>
-                    <TabButton selectTab={() => handleTabChange("Education")} active={tab === 'Education'}>
+                    <TabButton selectTab={() => handleTabChange("education")} active={tab === 'education'}>
                         {" "}
                         Education{" "}
                     </TabButton>
-                    <TabButton selectTab={() => handleTabChange("Certifications")} active={tab === 'Certifications'}>
+                    <TabButton selectTab={() => handleTabChange("certifications")} active={tab === 'certifications'}>
                         {" "}
                         Certifications{" "}
                     </TabButton>
                 </div>
+                <div className='mt-8'> {TAB_DATA.find((t) => t.id === tab).content} </div>
             </div>
         </div>
     </section>
