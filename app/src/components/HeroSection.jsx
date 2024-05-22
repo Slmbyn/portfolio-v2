78a10becaf32d6d865c79caa02ea08303c9c5008
SelamB <selambeyene2@gmail.com>
2024-05-21 18:32:56 -0700

    updates placeholder text in nav/footer

--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -44,9 +44,14 @@ const HeroSection = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fuga cum aperiam iusto voluptatum doloremque quo. Corrupti, recusandae voluptatibus!
                 </p>
                 <div>
-                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
-                        Resume
-                    </button>
+                    <a        
+                        href="https://drive.google.com/file/d/1eKl6XrAK0Y-vuqGhFCkhzQzLP8Qisjtz/view?usp=sharing"
+                        target="_blank"
+                        download
+                        className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white cursor-pointer'
+                    >
+                            Resume
+                    </a>
                     {/* <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white mt-3'>
                         <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                             Download CV
