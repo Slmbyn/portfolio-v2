a27d6caf0d45fdd29b7218430bf983b173020e4d
SelamB <selambeyene2@gmail.com>
2024-05-21 18:13:27 -0700

    centers text and hides about me image on mobile

--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -45,13 +45,13 @@ const HeroSection = () => {
                 </p>
                 <div>
                     <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
-                        Hire Me
+                        Resume
                     </button>
-                    <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white mt-3'>
+                    {/* <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white mt-3'>
                         <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                             Download CV
                         </span>
-                    </button>
+                    </button> */}
                 </div>
             </motion.div>
 
