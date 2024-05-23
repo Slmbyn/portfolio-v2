3b117ba4071bd9f5d090eecc0fe1cdf13439e442
SelamB <selambeyene2@gmail.com>
2024-05-22 18:42:54 -0700

    still delinting

--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
                 className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl font-extrabold"> 
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
-                        Hello, I&apos;m
+                        Hello, I&rsquo;m
                     </span>
                     <br></br>
                     <TypeAnimation
